373ca708a1aa5ab918adadfa8698081916814473
Mayur22103 <maxthakkar6200@gmail.com>
2024-05-17 11:14:04 +0530

    commit

---
+++ Input_Output.py
@@ -0,0 +1,21 @@
+# x=6
+# print(x)
+
+# name = input()
+# print(name)
+
+# x=6
+# y=9
+# salary=float(input())
+
+# print(x,end="-")
+# print(y)
+# print(type(x))
+# print(salary)
+# print(type(salary))
+
+# a,b,c = map(int,input().split(","))
+# print(a,b,c)
+
+lst = list(map(int,input().split(",")))
+print(lst)
